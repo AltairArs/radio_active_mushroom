c7fcccf30fc13e5270ef41e74075290914b4a17d
ssr14 <ssr14@tpu.ru>
2024-10-12 20:01:47 +0700

    create accounts service

---
+++ src/main/java/com/example/radio_active_mushroom/services/AccountsService.java
@@ -0,0 +1,4 @@
+package com.example.radio_active_mushroom.services;
+
+public interface AccountsService {
+}
---
+++ src/main/java/com/example/radio_active_mushroom/services/AccountsServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.radio_active_mushroom.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountsServiceImpl implements AccountsService {
+}
