3864cf14062fd46a9c80abe1b662f3e444cf0229
ssr14 <ssr14@tpu.ru>
2024-10-12 21:16:43 +0700

    edit login page

--- src/main/java/com/example/radio_active_mushroom/constraints/OnlyOneNotNullValidator.java
+++ src/main/java/com/example/radio_active_mushroom/constraints/OnlyOneNotNullValidator.java
@@ -11,14 +11,14 @@ public class OnlyOneNotNullValidator implements ConstraintValidator<OnlyOneNotNu
     @Override
     public void initialize(OnlyOneNotNull constraintAnnotation) {
         this.fields = constraintAnnotation.fields();
-        ConstraintValidator.super.initialize(constraintAnnotation);
     }
 
     @Override
     public boolean isValid(Object o, ConstraintValidatorContext constraintValidatorContext) {
         int count = 0;
         for (String field : fields) {
-            if (new BeanWrapperImpl(o).getPropertyValue(field) != null) {
+            Object value = new BeanWrapperImpl(o).getPropertyValue(field);
+            if (value != null && value != "") {
                 count++;
             }
         }
