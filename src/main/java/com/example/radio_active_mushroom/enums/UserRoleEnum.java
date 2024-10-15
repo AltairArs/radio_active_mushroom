2fa71922e677e8d1894489b409af60e9bc643b56
ssr14 <ssr14@tpu.ru>
2024-10-15 18:12:30 +0700

    add permissions algorithm

--- src/main/java/com/example/radio_active_mushroom/enums/UserRoleEnum.java
+++ src/main/java/com/example/radio_active_mushroom/enums/UserRoleEnum.java
@@ -9,13 +9,14 @@ public enum UserRoleEnum {
     ROLE_USER, ROLE_STAFF, ROLE_ADMIN, ROLE_MARKETER, ROLE_DB_DRAWER_EDITOR;
 
     private final static String[] permissions = new String[]{
-        "_U_S_A_M_D_PROFILE"
+            "_U_S_A_M_D__PROFILE",
+            "_U_S_A_M_D__LOGOUT"
     };
 
     private Collection<SimpleGrantedAuthority> getPermissions(String id) {
         return Arrays.stream(permissions)
             .filter(authority -> authority.contains(id))
-            .map(SimpleGrantedAuthority::new)
+            .map(permission -> new SimpleGrantedAuthority(permission.substring(permission.lastIndexOf("__") + 1)))
             .toList();
     }
 
